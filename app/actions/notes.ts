14a28dcf74bc58928a663bee11c8a1ed4adde8c0
Julidivi1 <gatmuz@gmail.com>
2026-08-26 14:40:31 -0500

    Segunda Parte - Toggle de Importancia

---
+++ app/actions/notes.ts
@@ -0,0 +1,23 @@
+"use server";
+
+import { redirect } from "next/navigation";
+import { revalidatePath } from "next/cache";
+import { addNote } from "../services/notes";
+import { toggleImportance } from "../services/notes";
+
+export const createNote = async (formData: FormData) => {
+  const content = formData.get("content") as string;
+  const important = formData.get("important") === "on";
+  addNote(content, important);
+
+  revalidatePath("/notes");
+  redirect("/notes");
+};
+
+export const toggleNoteImportance = async (formData: FormData) => {
+  const id = Number(formData.get("id"))
+  toggleImportance(id)
+  revalidatePath(`/notes/${id}`)
+  revalidatePath("/notes")
+  redirect("/notes");
+}
